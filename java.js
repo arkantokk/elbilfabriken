25ff662de7c7c096f1830e8c2bbbbbbeeb0e368f
WarpedTaters <bgrutchfield@gmail.com>
2026-04-23 10:10:07 +0200

    added more test files

--- java.js
+++ java.js
@@ -1,3 +1,13 @@
+var liste_over_valg=[]
+
+const button =document.getElementById("test")
+const button2 =document.getElementById("test2")
+const button3 =document.getElementById("test3")
+
+button.addEventListener("click", function() {
+  alert("test success");
+});
+
 function get_results(liste){
-    document.getElementById("choice1") //fill this in properly later
+
 }
