ac7b3d20b495ddc80fa315585f9fc6c318380377
WarpedTaters <bgrutchfield@gmail.com>
2026-04-27 08:23:16 +0200

    translation to english

--- java.js
+++ java.js
@@ -2,6 +2,7 @@ const button =document.getElementById("test")
 const button2 =document.getElementById("test2")
 const button3 =document.getElementById("test3")
 var choice1;
+const gjennomsnittsutslipp = 150;
 
 button.addEventListener("click", () => {
     choice1=button;
@@ -22,19 +23,38 @@ get_results(liste_over_valg)
 });
 
 function get_results(liste){
+    var utslipp = 0
+    var fabrikkpris = 0
     var text =document.getElementById("resultsinfo")
     textcontent=""
+    var label =document.getElementById("input")
+    var size = parseInt(label.value)
     if (liste[0]==button)
     {
-        text.textContent =+ "Du drepte 3000 kaniner med fabrikken din :("
+        textcontent += `you killed ${3000*size} rabbits with yor factory :( `
+        utslipp +=100
     }
         if (liste[0]==button2)
     {
-        text.textContent = "Du drepte 8000 kaniner med fabrikken din :("
+        textcontent += "You killed 8000 rabbits with yor factory :( "
+        utslipp+= 200
+        fabrikkpris += 200000
     }
-            if (liste[0]==button3)
+        if (liste[0]==button3)
     {
-        text.textContent = "Du drepte 100 kaniner med fabrikken din :("
+        textcontent += "You killed 100 rabbits with yor factory :( "
+        utslipp +=10
+    }
+    selector1=document.getElementById("selector")
+    textcontent += selector1.value
+    
+    textcontent += `and kost you ${fabrikkpris}kr` //vet ikke hvor viktig det er å inkludere pris? men inkluderte slik at vi har muligheten
+    textcontent += ` and your factory put out ${utslipp} ton CO2 per year`
+    if (utslipp < gjennomsnittsutslipp) 
+    {
+        utslippmin=gjennomsnittsutslipp-utslipp
+        textcontent += ` thats ${utslippmin} ton less than the average factory, most factories put out ${gjennomsnittsutslipp} ton CO2 per year...`
     }
 
+    text.textContent = textcontent
 }
